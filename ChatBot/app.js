5a995570f1aca9820b264e2c9000166da216b1a7
Ahmed Abdelmoneim <ahmedh.abdelmoneim@gmail.com>
2018-03-24 17:17:29 -0700

    added QnA maker and LUIS APIs

--- ChatBot/app.js
+++ ChatBot/app.js
@@ -1,6 +1,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var request = require('request');
+var cognitiveServices = require('botbuilder-cognitiveservices');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -14,41 +15,77 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MicrosoftAppPassword
 });
 
-// Listen for messages from users
-server.post('/api/messages', connector.listen());
+//Setting up QnA maker
+var qnarecognizer = new cognitiveServices.QnAMakerRecognizer({
+    knowledgeBaseId: '81484fa8-104e-417b-9ded-92952ae8d65d',
+    subscriptionKey: '873988af65bd498285a485f557f8e9d8'});
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+// Setting up LUIS Recognizer
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/3ef1158b-3f7b-4960-98f6-94d62531b0f4?subscription-key=98b6d6a72718470c98e1a91f2a29dc67&verbose=true&timezoneOffset=-480&q=';
+var recognizer = new builder.LuisRecognizer(model);
+
+// Receive messages from the user and start the first dialogue
 var bot = new builder.UniversalBot(connector, function (session) {
-    //session.send("You said: %s", session.message.text);
-     session.beginDialog('returnItem');
+    session.send("Hello, how can I help you");
+	session.beginDialog('/startD')
 });
 
 // Manually set storage
 bot.set('storage', new builder.MemoryBotStorage());
 
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Start first dialogue
+bot.dialog('/startD', intents, function (session, args, next) {
+	session.send('Hello!, how can I help you?')
+})
+
+// Add QnA and LUIS recognizers
+var intents = new builder.IntentDialog({ recognizers: [recognizer, qnarecognizer] });
+bot.dialog('/startD', intents);
+
+var qnaMakerDialog = new cognitiveServices.QnAMakerDialog({
+    recognizers: [qnarecognizer],
+    defaultMessage: 'Sorry, no match found!',
+    qnaThreshold: 0.3
+});
+
+// Response to the Return Item Intent
+intents.matches('Return Item', [
+	function (session, args) {
+		session.replaceDialog('returnItem');
+	}
+]);
+
+// Answering Questions in the QnA maker knowledge base
+intents.matches('qna', [
+    function (session, args, next) {
+        var answerEntity = builder.EntityRecognizer.findEntity(args.entities, 'answer');
+        session.send(answerEntity.entity);
+    }
+]);
+
 // Dialog for returning an item
 bot.dialog('returnItem', [
-    // Step 1
-    function (session) {
-        builder.Prompts.text(session, 'Hi! How Can I help you?');
-    },
     // Ask for ID number
     function (session) {
-        builder.Prompts.text(session, 'Please insert product ID number?');
+        builder.Prompts.text(session, "Please insert product ID number?");
     },
 
     // Ask for Date
     function(session, args) {
       var id = session.message.text;
-       if (id){
-           var url = 'http://localhost:8000/21372';
-           request(url,function(error,response,body){
-               body = JSON.parse(body);
-               temp = body;
-               session.send(temp);
-           });
-           builder.Prompts.text(session, "API Call. (CHANGE LATER!)");
-      }
+      //  if (id) {
+      //      var url = 'http://localhost:8000/21372';
+      //      request(url,function(error,response,body){
+      //          body = JSON.parse(body);
+      //          temp = body;
+      //          session.send(temp);
+      //      });
+      //      builder.Prompts.time(session, "Please provide purchase date)");
+      // }
+      builder.Prompts.time(session, "Please provide purchase date");
     },
 
     // Ask if item has been opened
@@ -61,7 +98,7 @@ bot.dialog('returnItem', [
       if (!results.response) {
         builder.Prompts.confirm(session, "Are you sure you want to return the item?");
       } else {
-        session.endDialog(`Sorry, this item is not returnable`);
+        session.endDialog('Sorry, this item is not returnable');
       }
     },
 
@@ -69,6 +106,14 @@ bot.dialog('returnItem', [
 
     // End Dialog
     function (session, results) {
-        session.endDialog(`Sorry, this item is not returnable`);
+        session.endDialog('Sorry, this item is not returnable');
     }
+]);
+
+
+// Answer if none of the intents match or no answer in QnA knowledge base
+intents.onDefault([
+  function(session){
+      session.send('Sorry, but we can not seem to find an answer to your question at the present moment. For further assistance with this question please call 1-866-237-8289.');
+	}
 ]);
--- ReturnableApi/app.js
+++ ReturnableApi/app.js
@@ -1,5 +1,6 @@
 var express        = require('express');
 var bodyParser     = require('body-parser');
+var request        = require('request');
 
 // Set up express app
 var app = express();
@@ -7,14 +8,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Home page
 app.get('/', function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.json(JSON.stringify({ a: "1" }, null, 3));
+  //res.setHeader('Content-Type', 'application/json');
+  //res.json(JSON.stringify({ a: "1" }, null, 3));
 })
 
-// Post Request with ID
+// Get Request with ID
 app.get('/:id', function(req, res, next){
-    console.log(req.params.id);
-    res.redirect("/");
+  var id = req.params.id;
+  var temp;
+  var url = 'https://bizhacks.bbycastatic.ca/mobile-si/si/v4/pdp/overview/' + id +'?lang=en';
+      request(url,function(error,response,body){
+            body = JSON.parse(body);
+            temp = body.categoryId;
+            //res.setHeader('Content-Type', 'application/json');
+            res.json(JSON.stringify({ categoryID: temp }, null, 3));
+            res.redirect("/");
+      });
 });
 
  // Start up server
